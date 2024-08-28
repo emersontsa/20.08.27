504fd97bb35f51db220587a42a0d95551205d9b4
aluno <aluno@udicentro.local>
2024-08-27 21:55:13 -0300

    nova classa teste

---
+++ Exe.branches.tags/exerciocio_cranches/src/teste.java
@@ -0,0 +1,20 @@
+public class Teste {
+    // Atributos da classe
+    private String mensagem;
+
+    // Construtor
+    public Teste(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    // Método para exibir a mensagem
+    public void exibirMensagem() {
+        System.out.println(mensagem);
+    }
+
+    // Método principal
+    public static void main(String[] args) {
+        Teste teste = new Teste("Olá, mundo!");
+        teste.exibirMensagem();
+    }
+}
